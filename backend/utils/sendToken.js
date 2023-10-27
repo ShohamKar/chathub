c96add266e67385caff359183b388923bdac8139
Shoham <kar.shoham.25@gmail.com>
2023-10-28 01:58:36 +0530

    ready to be deployed

---
+++ backend/utils/sendToken.js
@@ -0,0 +1,14 @@
+let sendToken = async (res, user, statusCode, message) => {
+  let token = await user.getJWT();
+  let options = {
+    httpOnly: true,
+    expires: new Date(Date.now() + 15 * 24 * 60 * 60 * 1000),
+  };
+  return res.status(statusCode).cookie("token", token, options).json({
+    success: true,
+    message,
+    user,
+  });
+};
+
+export default sendToken;
